1408bef36a61cbfa762c688cc8f4ca945fb93695
Unknown <astrid2242@gmail.com>
2017-03-29 11:32:45 +0800

    [data]1703291

--- Exp1_ver4/Exp_ver4.m
+++ Exp1_ver4/Exp_ver4.m
@@ -11,13 +11,13 @@ close all;
 
 %====== initial condition =====% 
     
-    faceOpc(1,1) = 1.0;  faceOpc(2,1) = 0.5;
-    faceOpc(1,2) = 1.0;  faceOpc(2,2) = 0.5;
-    faceOpc(1,3) = 1.0;  faceOpc(2,3) = 0.5;
-    faceOpc(1,4) = 1.0;  faceOpc(2,4) = 0.5;
+    faceOpc(1,1) = 1.0;  faceOpc(2,1) = 0.4;
+    faceOpc(1,2) = 1.0;  faceOpc(2,2) = 0.4;
+    faceOpc(1,3) = 1.0;  faceOpc(2,3) = 0.4;
+    faceOpc(1,4) = 1.0;  faceOpc(2,4) = 0.4;
 
-    maskOpc = 0.10;
-    disX = 180;
+    maskOpc = 0.15;
+    disX = 200;
     waitTime = 60;
 
     lowerBound = 0.02;
--- Exp1_ver4/thresholding.m
+++ Exp1_ver4/thresholding.m
@@ -19,8 +19,8 @@ close all;
     faceOpc(1,3) = 1;  faceOpc(2,3) = 0.4;
     faceOpc(1,4) = 1;  faceOpc(2,4) = 0.4;
 
-    maskOpc = 0.1;
-    disX = 180;
+    maskOpc = 0.5;
+    disX = 200;
     waitTime = 60;
 
     lowerBound = 0.02;
